ff0d60048bfc8d36b6dbc50ab756913935265bb1
Edson Jaramillo <edsonajaramillo@gmail.com>
2026-09-02 08:29:30 -0500

    fix title

--- apps/quotes/src/routes/__root.tsx
+++ apps/quotes/src/routes/__root.tsx
@@ -10,7 +10,7 @@ export const Route = createRootRoute({
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { title: "TanStack Start Starter" },
+      { title: "Kappler Quotes Demo" },
     ],
     links: [{ rel: "stylesheet", href: adminCss }],
   }),
